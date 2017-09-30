1e2c1015c89979d443dcd8331e168b0573516476
natsachaj <natascha_jakobsen@hotmail.com>
2017-09-30 18:56:24 +0200

    Add project files.

---
+++ XMLClient/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLib;
+using System.Net.Sockets;
+using System.IO;
+using System.Xml.Serialization;
+using System.Net;
+
+namespace XMLClient
+{
+    public class Client
+    {
+        public void Start()
+        {
+            Car publicCar = new Car();
+            publicCar.Model = "Tesla";
+            publicCar.RegNo = "XMLCar23";
+            publicCar.Color = "Blue";
+
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 10002);
+            listener.Start();
+
+            using(TcpClient tc = listener.AcceptTcpClient())
+            using (NetworkStream ns = tc.GetStream())
+            using (StreamWriter sw = new StreamWriter(ns))
+            {
+                XmlSerializer xmlSerial = new XmlSerializer(typeof(Car));
+
+                xmlSerial.Serialize(sw, publicCar);
+            }
+        }
+        }
+    }
+}
---
+++ XMLServer/Server.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ModelLib;
+
+namespace XMLServer
+{
+    public class Server
+    {
+        public void Start()
+        {
+            TcpListener server = new TcpListener(IPAddress.Loopback, 10002);
+            server.Start();
+
+          
+
+            while (true)
+            {
+                using (TcpClient client = server.AcceptTcpClient())
+                using (NetworkStream ns = client.GetStream())
+                using (StreamReader sr = new StreamReader(ns))
+                {
+
+
+                    Console.WriteLine("tjekker connection...");
+
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+
+
+                    //XmlSerializer xmlserial = new XmlSerializer(typeof(Car));
+                    //Car bil = (Car)xmlserial.Deserialize(ns);
+                    //xmlserial.Deserialize(sr);
+                    //Console.ReadKey();
+
+                    //using (NetworkStream ns = client.GetStream())
+                    //using (StreamReader sr = new StreamReader(ns))
+                    //using (StreamWriter sw = new StreamWriter(ns))
+                    //{
+                    //    string line = sr.ReadLine();
+                    //    Console.WriteLine(line);
+                    //}
+                }
+            }
+        }
+    }
+}
